e8a198cc5e270e1132493064f0e7374205576fb8
axelio <axel.szweda@gmail.com>
2021-04-03 13:56:41 +0200

    remove not needed methods

--- Settlements/Program.cs
+++ Settlements/Program.cs
@@ -9,8 +9,6 @@ namespace Settlements
     {
         private static void Main()
         {
-            var paymentsInfo = new List<string>();
-            
             var persons = new List<Person>
             {
                 new("A", 30.00m, 40.20m),
@@ -20,19 +18,68 @@ namespace Settlements
                 new("E", 20.00m, 20.00m)
             };
 
-            PrinterService.PrintBalanceInfo(persons);
+            if (persons.Sum(p => p.TotalIn) != persons.Sum(p => p.TotalOut))
+                throw new ArgumentException("Invalid total in and total out of all people balance.");
+
+            ShowBalanceInfo(persons);
             WriteLine();
-            ValidateTotals(persons);
-            CalculatorService.SettlePayments(persons, paymentsInfo);
-            PrinterService.PrintPaymentsInfo(paymentsInfo);
+            ShowPayments(persons);
 
             ReadKey();
         }
 
-        private static void ValidateTotals(List<Person> persons)
+        public static void ShowBalanceInfo(IEnumerable<Person> persons)
         {
-            if (persons.Sum(p => p.TotalIn) != persons.Sum(p => p.TotalOut))
-                throw new ArgumentException("Invalid total in and total out of all people balance.");
+            foreach (var p in persons)
+            {
+                WriteLine($"Name: {p.Name} \t Total in: {p.TotalIn} \t Total out: {p.TotalOut} \t TotalBalance: {p.TotalBalance}");
+            }
+        }
+
+        public static void ShowPayments(IEnumerable<Person> persons)
+        {
+            var (positiveBalancePersons, negativeBalancePersons) = GetGroupedPersons(persons);
+
+            foreach (var loser in negativeBalancePersons)
+            {
+                foreach (var winner in positiveBalancePersons)
+                {
+                    if (winner.IsSettled) continue;
+
+                    var loserCurrentBalanceAbsoluteValue = Math.Abs(loser.CurrentBalance);
+
+                    if (winner.CurrentBalance >= loserCurrentBalanceAbsoluteValue)
+                    {
+                        WriteLine($"{loser.Name} owes {winner.Name}: {loserCurrentBalanceAbsoluteValue}");
+                        winner.CurrentBalance -= loserCurrentBalanceAbsoluteValue;
+                        loser.CurrentBalance = 0;
+                        break;
+                    }
+
+                    if (winner.CurrentBalance < loserCurrentBalanceAbsoluteValue)
+                    {
+                        WriteLine($"{loser.Name} owes {winner.Name}: {winner.CurrentBalance}");
+                        loser.CurrentBalance += winner.CurrentBalance;
+                        winner.CurrentBalance = 0;
+                    }
+                }
+            }
+        }
+
+        private static (List<Person> positiveBalancePersons, List<Person> negativeBalancePersons) GetGroupedPersons(IEnumerable<Person> persons)
+        {
+            var sortedPersons = persons.OrderByDescending(p => p.TotalBalance);
+
+            var positiveBalancePersons = new List<Person>();
+            var negativeBalancePersons = new List<Person>();
+
+            foreach (var p in sortedPersons)
+            {
+                if (p.TotalBalance > 0) positiveBalancePersons.Add(p);
+                else if (p.TotalBalance < 0) negativeBalancePersons.Add(p);
+            }
+
+            return (positiveBalancePersons, negativeBalancePersons);
         }
     }
 }
